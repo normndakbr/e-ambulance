8cddbef482c0bd9d208e8d44ecb0cec198b6d33c
Normandia Akbar <normnd.akbr2@gmail.com>
2022-12-12 07:48:51 +0800

    updated, added filter button

--- e_ambulance_apps/lib/pages/riwayat.dart
+++ e_ambulance_apps/lib/pages/riwayat.dart
@@ -26,14 +26,14 @@ class _RiwayatState extends State<Riwayat> {
             ),
             Container(
               alignment: Alignment.topLeft,
-              padding: EdgeInsets.only(left: width * 0.08),
+              padding: EdgeInsets.only(left: width * 0.06),
               child: Text(
                 'Riwayat Perjalanan',
                 textAlign: TextAlign.left,
                 style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w700,
-                  fontSize: height * 0.030,
+                  fontSize: height * 0.038,
                 ),
               ),
             ),
@@ -41,14 +41,17 @@ class _RiwayatState extends State<Riwayat> {
               height: height * 0.03,
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
               child: Row(
                 children: [
                   SizedBox(
                     width: width * 0.7,
                     child: TextField(
                       decoration: InputDecoration(
-                        // prefixIcon: Icon(Icons.search),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: primaryColor,
+                        ),
                         filled: true,
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30.0),
@@ -58,8 +61,48 @@ class _RiwayatState extends State<Riwayat> {
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: width * 0.06,
+                  ),
+                  SizedBox(
+                    height: 53,
+                    width: 53,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      // child: PesananKosong(height: height, width: width),
+                      child: Image(
+                        height: 50,
+                        width: 50,
+                        image: AssetImage('assets/images/btnFilter.png'),
+                      ),
+                    ),
+                  ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: height * 0.03,
+            ),
+            SingleChildScrollView(
+              child: ListView.builder(
+  padding: const EdgeInsets.all(8),
+  itemCount: entries.length,
+  itemBuilder: (BuildContext context, int index) {
+    return Container(
+      height: 50,
+      color: Colors.amber[colorCodes[index]],
+      child: Center(child: Text('Entry ${entries[index]}')),
+    );
+  }
+,
             ),
           ],
         ),
